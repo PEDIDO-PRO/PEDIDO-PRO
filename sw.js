18d2cb3cad2070da6c44225679a55b3aa2a606f5
LUCAS SIQUEIRA <lucasdebarrossiqueira@gmail.com>
2026-09-13 17:51:33 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "pedido-pro-v3";
+const ARQUIVOS = [
+"./index.html",
+"./manifest.json",
+"./icons/icon-192.png",
+"./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS))
+);
+self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+event.waitUntil(
+caches.keys().then((nomes) =>
+Promise.all(
+nomes.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+)
+)
+);
+self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+if (event.request.method !== "GET") return;
+
+event.respondWith(
+fetch(event.request)
+.then((resp) => {
+const respClone = resp.clone();
+caches.open(CACHE_NAME).then((cache) => cache.put(event.request, respClone));
+return resp;
+})
+.catch(() => caches.match(event.request))
+);
+});
